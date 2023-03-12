0e8fe8c125c5524fc84389b843d6622d9645b3c3
mengweij <mengweij@student.ubc.ca>
2023-03-11 17:08:36 -0800

    React Calendar setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
+
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 import './App.css';
 
 function App() {
+  const [date, setDate] = useState(new Date());
+  //The initial value passed to the calendar is the present date.
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1 className='text-center'>What's your mood today?</h1>
+      <div className='calendar-container'>
+        <Calendar 
+          onChange={setDate} 
+          value={date} 
+          //maxDate={new date()}
+          //minDate={new Date(2023, 1, 1)}
+        />
+      </div>
+      <p className='text-center'>
+        <span className='bold'>Selected Date:</span>{' '}
+        {date.toDateString()}
+      </p>
     </div>
   );
 }
